ee8a3545abc9ee82d325165dea7aac1e5cce0460
Gustavo D. Tonetto <gustavotonetto1@gmail.com>
2023-08-18 22:07:27 -0300

    Criando as funções de login e tratamneto de login

--- src/main/java/br/com/senai/usuariosmarketplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmarketplace/InitApp.java
@@ -1,9 +1,15 @@
 package br.com.senai.usuariosmarketplace;
 
+import br.com.senai.usuariosmarketplace.core.service.UsuarioService;
+
 public class InitApp {
 
 	public static void main(String[] args) {
-		
+		UsuarioService service = new UsuarioService();
+		System.out.println(service.removerAcentoDo("João da Silva"));
+		System.out.println(service.fracionar("João da Silva e Albuquerque"));
+		System.out.println(service.gerarLoginPor("José da PQP"));
+		System.out.println(service.gerarHashDa("12312312"));
 	}
 
 }
