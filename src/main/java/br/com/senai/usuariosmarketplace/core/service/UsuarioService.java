b7f73c53a7f2921f6168a981a600a8fde75fc4c9
Gustavo D. Tonetto <gustavotonetto1@gmail.com>
2023-08-24 20:49:18 -0300

    Arrumando problema de espaço em branco na criação do usuario

--- src/main/java/br/com/senai/usuariosmarketplace/core/service/UsuarioService.java
+++ src/main/java/br/com/senai/usuariosmarketplace/core/service/UsuarioService.java
@@ -39,7 +39,7 @@ public class UsuarioService {
 	@SuppressWarnings("null")
 	private List<String> fracionar(String nomeCompleto) {
 		List<String> nomeFracionado = new ArrayList<String>();
-		if (nomeCompleto != null || nomeCompleto.isBlank()) {
+		if (!Strings.isNullOrEmpty(nomeCompleto)) {
 			nomeCompleto = nomeCompleto.trim();
 			String[] partesDoNome = nomeCompleto.split(" ");
 			for (String parte : partesDoNome) {
