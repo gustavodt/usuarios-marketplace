3575adc50c18f934cba826a91977a4a93e576a6e
Gustavo D. Tonetto <gustavotonetto1@gmail.com>
2023-08-24 22:14:53 -0300

    Utilização do lombok

--- src/main/java/br/com/senai/usuariosmarketplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmarketplace/InitApp.java
@@ -1,9 +1,15 @@
 package br.com.senai.usuariosmarketplace;
 
+import br.com.senai.usuariosmarketplace.core.dao.ManagerDb;
+import br.com.senai.usuariosmarketplace.core.domain.Usuario;
+
 public class InitApp {
 
 	public static void main(String[] args) {
-		
+		ManagerDb.getInstance().getConexao();
+		System.out.println("Conecotu");
+		Usuario usuario = new Usuario("jose.silva", "José da Silva", "jose23232");
+		System.out.println(usuario.getLogin());
 	}
 
 }
